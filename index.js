3d07f50f895935d9ba45e6c512921ae9f7fca3f0
andyjohn23 <andrewnick906@gmail.com>
2024-05-11 13:15:34 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// let message = "Hello guys!";
+
+// function sayHello() {
+//     console.log(message);
+//     let personName = "Joel";
+//     console.log(personName);
+// }
+
+// if (message == "Hello guys!") {
+//     let message2 = "Hello"
+//     console.log("You got it")
+//     console.log(message2);
+// } else {
+//     console.log("not yet")
+// }
+
+// console.log(sayHello());
+
+// let goals2023 = ["level up azure", "getting fit", "finances", "personal development", 2024]
+// console.log(goals2023[0])
+// console.log(goals2023[1])
+// goals2023[0] = "level up javascript";
+// goals2023[1] = "working of finances";
+// console.log(goals2023.length);
+// console.log(goals2023);
+
+// goals2023.push("testing this")
+// goals2023.pop();
+// goals2023.unshift("testing1", "testing2")
+// console.log(goals2023)
+// goals2023.shift()
+// console.log(goals2023)
+
+let goals2023 = [
+    {
+        goalName: "level up on azure",
+        duration: "month",
+        time: 2,
+        achieved: true
+    }
+]
+console.log(goals2023[0].goalName)
+
+
+// console.log(goals)
